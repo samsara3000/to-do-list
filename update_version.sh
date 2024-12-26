e1b0d2bf78f61a2dba36511a95f35be175ee5a9f
ruslan.gomzyakov <ruslan.gomziakov@math.msu.ru>
2024-12-26 14:33:49 +0300

    update update_version.sh

--- update_version.sh
+++ update_version.sh
@@ -5,7 +5,7 @@ CURRENT_VERSION=$(cat version.txt)
 
 # Увеличить версию
 VERSION_MAJOR=${CURRENT_VERSION#}
-NEW_VERSION="$VERSION_MAJOR"
+NEW_VERSION="$VERSION_MAJOR + 1"
 
 # Записать новую версию в файл
 echo $NEW_VERSION > version.txt
