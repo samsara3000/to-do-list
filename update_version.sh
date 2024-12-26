bfa0143779236d32839e4fdc602977c1de251d28
ruslan.gomzyakov <ruslan.gomziakov@math.msu.ru>
2024-12-26 13:11:56 +0300

    change script

--- update_version.sh
+++ update_version.sh
@@ -6,12 +6,10 @@ CURRENT_VERSION=$(cat version.txt)
 # Разделить версию на компоненты
 IFS='.' read -r -a VERSION_PARTS <<< "$CURRENT_VERSION"
 VERSION_MAJOR=${VERSION_PARTS[0]}
-VERSION_MINOR=${VERSION_PARTS[1]}
-VERSION_PATCH=${VERSION_PARTS[2]}
 
 # Увеличить версию
 VERSION_PATCH=$((VERSION_PATCH + 1))
-NEW_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
+NEW_VERSION="${VERSION_MAJOR}"
 
 # Записать новую версию в файл
 echo $NEW_VERSION > version.txt
